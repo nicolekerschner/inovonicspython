ddf797cdfcd855fd94aa9657ffeb025e5cb2daa3
nicolekerschner <npkerschner01@bvsd.org>
2021-09-16 14:46:11 -0600

    I really am frustrated trying to make a stupid button (9/16)

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
 import PySimpleGUI as sg
 
-sg.theme('LightGrey2')   # Add a touch of color
+sg.theme('LightGreen')
+# Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Text('Some text on Row 1')],
             [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+            [sg.Button('Ok'), sg.Button('Cancel')],
+            [sg.Button('Beep')]]
 
 # Create the Window
 window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
+    if event == 'Beep':
+        print('beep')
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    if event == 'Ok':
+        print('You entered ', values[0])
+
 
 window.close()
