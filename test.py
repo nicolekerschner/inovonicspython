74d8592306b4b2987bdac369b1f8ee11b7db1117
nicolekerschner <npkerschner01@bvsd.org>
2021-09-23 15:01:41 -0600

    Really frustrated on trying to make a table (9/23)

--- test.py
+++ test.py
@@ -3,13 +3,24 @@ import PySimpleGUI as sg
 sg.theme('LightGreen')
 # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')],
-            [sg.Button('Beep')]]
+# layoutTable = []
+rows, cols = (5, 5)
+arr = [[0]*cols]*rows
 
+layout1 = [  [sg.Text('Some text on Row 1', font='Arial')],
+            [sg.Text('Enter something on Row 2', font='Helvetica'), sg.InputText()],
+            [sg.Button('Ok', font='Helvetica'), sg.Button('Cancel', font='Helvetica')],
+            [sg.Button('Beep', size=(4,1))],
+            [sg.Table
+             (values=arr, num_rows = 2, visible = True, select_rows = [1, 2], alternating_row_color ='Red', row_colors ='White')]
+             ]
+layout2 = [[sg.Text('Testing 1 2 3', font='Helvetica')]]
+layout3 = [[sg.Text('Beep Bop Boop')]]
+tabgrp = [[sg.TabGroup([[sg.Tab('Tab1', layout1, font='Helvetica'),
+                         sg.Tab('Tab2', layout2, font='Helvetica'),
+                         sg.Tab('Tab3', layout3, font='Helvetica')]], tab_location='topleft', border_width=5), sg.Button('Close')]]
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Window Title', tabgrp)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
