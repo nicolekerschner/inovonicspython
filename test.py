90d180f7135fb12d8d7000005c3ac5624295cc36
nicolekerschner <npkerschner01@bvsd.org>
2021-10-14 15:01:11 -0600

    Working on Tab1 (10/14)

--- test.py
+++ test.py
@@ -6,43 +6,80 @@ sg.theme('LightGreen')
 # All the stuff inside your window.
 # layoutTable = []
 rows, cols = (5, 5)
-arr = [[4]*cols]*rows
+arr = [[4] * cols] * rows
 print(arr)
-text = "hello"
-
 
 data = arr
-headings = [str(data[0][x]) for x in range(len(data[0]))]
+headings = ['hello', 'hello','hello', 'hello', 'hello']
 
-layout1 = [  [sg.Text(text, font='Arial')],
-            [sg.Text('Enter something on Row 2', font='Helvetica'), sg.InputText()],
-            [sg.Button('Ok', font='Helvetica'), sg.Button('Cancel', font='Helvetica')],
-            [sg.Button('Beep', size=(4,1))],
-           [sg.Table(values=data[1:][:], headings=headings, max_col_width=100, background_color='lightblue',
-                    display_row_numbers=True,
-                    justification='right',
-                    num_rows=rows,
-                    alternating_row_color='lightyellow',
-                    tooltip='This is a table')]
-             ]
+
+UID= [[sg.Text('UID', font='Arial')],
+           [sg.Text('Orginator', font='Helvetica'), sg.InputText(size=10)],
+           [sg.Text('First Hop', font='Helvetica'), sg.InputText(size=10)],
+           [sg.Text('Trace Count', font='Helvetica'), sg.InputText(size=10)],
+           [sg.Text('Trace UID', font='Helvetica'), sg.InputText(size=10)],
+           [sg.Text('Trace UID', font='Helvetica'), sg.InputText(size=10)],
+           [sg.Text('Trace UID', font='Helvetica'), sg.InputText(size=10)],
+           [sg.Text('Trace UID', font='Helvetica'), sg.InputText(size=10)],
+           [sg.Text('Trace UID', font='Helvetica'), sg.InputText(size=10)],
+
+          # [sg.Button('Ok', font='Helvetica'), sg.Button('Cancel', font='Helvetica')],
+          # [sg.Button('Beep', size=(4, 1))],
+           ]
+originInfo = [[sg.Text('Orignator Info', font='Arial')],
+              [sg.Text('Survery Bit Set', font='Helvetica')],
+              [sg.Text('Payload ID Present', font='Helvetica')],
+              [sg.Text('PTI Byte Present', font='Helvetica')],
+              [sg.Text('Device Class', font='Helvetica'), sg.InputText(size=10)],
+              [sg.Text('Device Type', font='Helvetica'), sg.InputText(size=10)],
+              [sg.Text('Payload ID', font='Helvetica'), sg.InputText(size=10)],
+              [sg.Text('Low Battery', font='Helvetica')],
+              [sg.Text('Supervision', font='Helvetica')],
+              [sg.Text('Reset', font='Helvetica')],
+              [sg.Text('Case Tamper', font='Helvetica')],
+              ]
+receiveStatus = [[sg.Text('Serial Receiver Status')],
+                 [sg.InputText(size=5), sg.Text('Messages', font="Helvetica")],
+                 [sg.Text('Receiver Jammed', font= 'Helvetica')],
+                 [sg.Text('Case Tamper', font= 'Helvetica')],
+                 [sg.Text('Supervisory', font= 'Helvetica')],
+                 [sg.Text('Processor Reset', font= 'Helvetica')],
+                 [sg.Text('Link Failure', font= 'Helvetica')],
+                 [sg.Button("Get RF Parameters")],
+                 [sg.Button('Get RX Settings')]]
+
+receiveConfig = [[sg.Text('Serial Receiver Configure')],
+                 [sg.Button('Inbound Complete')],
+                 [sg.Button('Inbound Verbatim')],
+                 [sg.Button('Security Extended')]]
+
+layout1 = [[sg.Column(UID),
+           sg.Column(originInfo),
+           sg.Column(receiveStatus),
+           sg.Column(receiveConfig)]]
 layout2 = [[sg.Text('Testing 1 2 3', font='Helvetica')]]
 layout3 = [[sg.Text('Beep Bop Boop')]]
-layout4 = [[sg.Table(values=data, headings=headings, max_col_width=20, background_color='lightblue',
-                    display_row_numbers=False,
-                    justification='right',
-                    auto_size_columns=True,
-                    num_rows=10,
-                    hide_vertical_scroll= True,
-                    expand_y=True,
-                    alternating_row_color='lightyellow',
-                    size=(100,100),
-                    tooltip='This is a table')]]
+layout4 = [[sg.Table(values=data,
+                     headings=headings,
+                     max_col_width=20,
+                     background_color='lightblue',
+                     display_row_numbers=False,
+                     col_widths=10,
+                     justification='left',
+                     auto_size_columns=True,
+                     num_rows=10,
+                     hide_vertical_scroll=True,
+                     expand_y=True,
+                     size=(100, 100),
+                     tooltip='This is a table')]]
+layout5 = [[sg.Text("Transmitter")]]
 
-tabgrp = [[sg.TabGroup([[sg.Tab('Tab1', layout1, font='Helvetica'),
-                         sg.Tab('Tab2', layout2, font='Helvetica'),
-                         sg.Tab('Tab3', layout3, font='Helvetica'),
-                         sg.Tab('Tab4', layout4, font='Helvetica')]], tab_location='topleft', border_width=5), sg.Button('Close')]]
-
+tabgrp = [[sg.TabGroup([[sg.Tab('Device Data', layout1, font='Helvetica'),
+                         sg.Tab('Environmental', layout2, font='Helvetica'),
+                         sg.Tab('Security', layout3, font='Helvetica'),
+                         sg.Tab('Submetering', layout4, font='Helvetica'),
+                         sg.Tab('CENELEC', layout5, font='Helvetica')]], tab_location='topleft', border_width=5),
+                         sg.Button('Close')]]
 
 # Create the Window
 window = sg.Window('Window Title', tabgrp)
@@ -51,10 +88,9 @@ while True:
     event, values = window.read()
     if event == 'Beep':
         print('beep')
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
         break
     if event == 'Ok':
         print('You entered ', values[0])
-
 
 window.close()
