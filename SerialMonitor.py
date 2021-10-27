1175e7de458fe20464f55e3b60e1b65b8bf4f5db
nicolekerschner <npkerschner01@bvsd.org>
2021-10-27 15:00:25 -0600

    Making Events and very close to done with skeleton (10/27)

--- SerialMonitor.py
+++ SerialMonitor.py
@@ -2,14 +2,10 @@ import PySimpleGUI as sg
 
 sg.theme('DefaultNoMoreNagging')
 
-# creating filler table
-# rows, cols = (5, 5)
-# arr = [[4] * cols] * rows
-# data = arr
-# headings = ['hello', 'hello','hello', 'hello', 'hello']
-
 headerFont = ('Helvetica', 10, 'bold')
 normalFont = ('Helvetica', 11)
+
+# SCREEN 1: DEVICE DATA
 UID = [[sg.Text('UID', font=headerFont)],
        [sg.Text('Orginator', font=normalFont), sg.InputText(size=10)],
        [sg.Text('First Hop', font=normalFont), sg.InputText(size=10)],
@@ -39,7 +35,8 @@ receiveStatus = [[sg.Text('Serial Receiver Status', font=headerFont)],
                  [sg.Text('Supervisory', font=normalFont)],
                  [sg.Text('Processor Reset', font=normalFont)],
                  [sg.Text('Link Failure', font=normalFont)],
-                 [sg.Button("Get RF Parameters")],
+                 [sg.Checkbox('Log Rx Status')],
+                 [sg.Button("Get RF Parameters", key='-RFPARA-')],
                  [sg.Button('Get RX Settings')]]
 
 receiveConfig = [[sg.Text('Serial Receiver Configure', font=headerFont)],
@@ -47,45 +44,73 @@ receiveConfig = [[sg.Text('Serial Receiver Configure', font=headerFont)],
                  [sg.Button('Inbound Verbatim')],
                  [sg.Button('Security Extended')]]
 
+displayMenu = ['All', 'Environmental', 'Security', 'Submetering', 'Repeater']
+
+display = [[sg.Text("Display:", font=normalFont)],
+           [sg.OptionMenu(displayMenu, default_value='All')]]
+
 messages = [[sg.Text('Messages/Sec', font=headerFont)],
             [sg.InputText(size=3)]]
 
 layout1 = [[sg.Column(UID, vertical_alignment='top'),
-           sg.Column(originInfo, vertical_alignment='top'),
-           sg.Column(receiveStatus, vertical_alignment='top'),
-           sg.Column(receiveConfig, vertical_alignment='top'),
-           sg.Column(messages, vertical_alignment='top')]]
+            sg.VerticalSeparator(),
+            sg.Column(originInfo, vertical_alignment='top'),
+            sg.VerticalSeparator(),
+            sg.Column(receiveStatus, vertical_alignment='top'),
+            sg.VerticalSeparator(),
+            sg.Column(receiveConfig, vertical_alignment='top'),
+            sg.VerticalSeparator(),
+            sg.Column(messages, vertical_alignment='top')],
+           [sg.Column(display, vertical_alignment='top')]]
 
+# SCREEN 2: ENVIRONMENTAL
 enviroHeadings = ['Device', ' MID ', ' H ', ' M ', ' L ', ' CH1 ', ' CH2 ', ' TX ', ' Meas ',
                   'Delta', ' Int ', ' Ext ', 'Level', 'Margin', 'Status', 'Timestamp']
 rows1, cols1 = (20, 16)
 arr1 = [[""] * cols1] * rows1
 
+monitorColor = "red"
+monitorText = "Monitor OFF"
+monitorOn = False
+
 layout2 = [
     [sg.Table(values=arr1, headings=enviroHeadings, vertical_scroll_only=True, alternating_row_color='lightBlue')],
-    [sg.Button('Register')],
-    [sg.Button('Monitor', button_color='red')]]
+    [sg.Button('Register'), sg.Button(monitorText, button_color=monitorColor, key='-MONITOR-')],
+    [sg.Checkbox("Analog Sensor"), sg.Checkbox("Log Raw Data")]]
 
+# SCREEN 3: SECURITY
 securityHeadings = ['Device', '  MID  ', '  IDH  ', '  IDM  ', '  IDL  ', '  Lvl  ', '  Mgn  ', ' Status ', 'Timestamp',
                     '  PHR  ', ' RNDS ', 'Alarm Rnds', 'Al MS', 'Super Count', 'Sp Ms', 'Sup. Sts']
 rows2, cols2 = (20, 16)
 arr2 = [[""] * cols2] * rows2
 
+monitorColor2 = "red"
+monitorText2 = "Monitor OFF"
+monitorOn2 = False
+
 layout3 = [[sg.Table(values=arr2, headings=securityHeadings, vertical_scroll_only=True,
                      alternating_row_color='lightBlue')],
-           [sg.Button('Register')],
-           [sg.Button('Monitor', button_color='red')]]
+           [sg.Button('Register'), sg.Button(monitorText2, button_color=monitorColor2, key="-MONITOR2-"),
+            sg.Checkbox("Log Raw Data")],
+           [sg.Text("TX Hit Rate")],
+           [sg.Text("Diagnostics")]]
 
+# SCREEN 4: SUBMETERING
 submeteringHeadings = ['Device', '  MID  ', '  SNH  ', '  SNM  ', 'Total Count', 'Leak Det', 'Level', 'Margin',
                        'Status', 'Timestamp']
 rows3, cols3 = (20, 11)
 arr3 = [[""] * cols3] * rows3
 
+monitorColor3 = "red"
+monitorText3 = "Monitor OFF"
+monitorOn3 = False
+
 layout4 = [[sg.Table(values=arr3, headings=submeteringHeadings, vertical_scroll_only=True,
                      alternating_row_color='lightBlue')],
            [sg.Button('Register')],
-           [sg.Button('Monitor', button_color='red')]]
+           [sg.Button(monitorText3, button_color=monitorColor3, key="-MONITOR3-")]]
 
+# SCREEN 5: CENELEC
 testTransmitter = [[sg.Text('Test Transmitter', font=headerFont)],
                    [sg.Text('MID', font=normalFont), sg.InputText(size=5)],
                    [sg.Text('IDH', font=normalFont), sg.InputText(size=5)],
@@ -111,13 +136,20 @@ messageNum = [[sg.Text('Message Number', font=headerFont), sg.InputText(size=5)]
               [sg.Text('Message Hit Rate %', font=normalFont), sg.InputText(size=5)],
               [sg.Text('Rounds', font=normalFont), sg.InputText(size=5)]]
 
-messageCount = [[sg.Text('Message Counting', font = headerFont)],
-                [sg.Button('Off', button_color='red')]]
+messageColor = "red"
+messageText = "OFF"
+messageOn = False
+
+messageCount = [[sg.Text('Message Counting', font=headerFont)],
+                [sg.Button(messageText, button_color=messageColor, key="-MESSAGE-")]]
 
 layout5 = [[sg.Column(testTransmitter, vertical_alignment='top'),
-           sg.Column(repeater, vertical_alignment='top'),
-           sg.Column(messageNum, vertical_alignment='top'),
-           sg.Column(messageCount, vertical_alignment='top')]]
+            sg.VerticalSeparator(),
+            sg.Column(repeater, vertical_alignment='top'),
+            sg.VerticalSeparator(),
+            sg.Column(messageNum, vertical_alignment='top'),
+            sg.VerticalSeparator(),
+            sg.Column(messageCount, vertical_alignment='top')]]
 
 top_menu_def = [['File', ['Close']],
                 ['Ports', ['Com No.', 'Settings']],
@@ -140,4 +172,60 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
         break
+    if event == '-MONITOR-':
+        if not monitorOn:
+            monitorColor = "green"
+            monitorText = "Monitor ON"
+            window['-MONITOR-'].update(button_color=monitorColor)
+            window['-MONITOR-'].update(monitorText)
+            monitorOn = True
+        else:
+            monitorColor = "red"
+            monitorText = "Monitor OFF"
+            window['-MONITOR-'].update(button_color=monitorColor)
+            window['-MONITOR-'].update(monitorText)
+            monitorOn = False
+
+    if event == '-MONITOR2-':
+        if not monitorOn2:
+            monitorColor2 = "green"
+            monitorText2 = "Monitor ON"
+            window['-MONITOR2-'].update(button_color=monitorColor2)
+            window['-MONITOR2-'].update(monitorText2)
+            monitorOn2 = True
+        else:
+            monitorColor2 = "red"
+            monitorText2 = "Monitor OFF"
+            window['-MONITOR2-'].update(button_color=monitorColor2)
+            window['-MONITOR2-'].update(monitorText2)
+            monitorOn2 = False
+
+    if event == '-MONITOR3-':
+        if not monitorOn3:
+            monitorColor3 = "green"
+            monitorText3 = "Monitor ON"
+            window['-MONITOR3-'].update(button_color=monitorColor3)
+            window['-MONITOR3-'].update(monitorText3)
+            monitorOn3 = True
+        else:
+            monitorColor3 = "red"
+            monitorText3 = "Monitor OFF"
+            window['-MONITOR3-'].update(button_color=monitorColor3)
+            window['-MONITOR3-'].update(monitorText3)
+            monitorOn3 = False
+
+    if event == '-MESSAGE-':
+        if not messageOn:
+            messageColor = "green"
+            messageText = "ON"
+            window['-MESSAGE-'].update(button_color=messageColor)
+            window['-MESSAGE-'].update(messageText)
+            messageOn = True
+        else:
+            messageColor = "red"
+            messageText = "OFF"
+            window['-MESSAGE-'].update(button_color=messageColor)
+            window['-MESSAGE-'].update(messageText)
+            messageOn = False
+
 window.close()
